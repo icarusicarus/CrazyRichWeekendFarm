105824551108698e916e12c251cf7135715d8238
icaruswithoutwings <bluemonster@pusan.ac.kr>
2021-04-12 18:11:04 +0900

    fix NIL error and change type string to enum

--- data.py
+++ data.py
@@ -1,13 +1,13 @@
-# Data Type
-#   1: int
-#   2: pair(in python, Tuple)
-#   3: nil(in python, None)
-#   4: symbol
+from enum import Enum
 
-Types = ['Int', 'Pair', 'Nil', 'Symbol']
+class Type(Enum):
+    NIL = 0
+    INT = 1
+    PAIR = 2
+    SYMBOL = 3
 
 class Data:
-    def __init__(self, type=0, value=0):
+    def __init__(self, type=Type.NIL, value=0):
         self.type = type
         self.value = value
     def car(self):
@@ -15,7 +15,9 @@ class Data:
     def cdr(self):
         return self.value[1]
     def __str__(self):
-        if self.type == 2:
+        if self.type == Type.NIL:
+            return "NIL"
+        elif self.type == Type.PAIR:
             try:
                 if(self.cdr() == int(self.cdr())):
                     return "("+str(self.car())+" . "+str(self.cdr())+")"
@@ -23,8 +25,8 @@ class Data:
                 retStr = '('
                 retStr+=str(self.car())
                 atom = self.cdr()
-                while(str(atom) != 'None'):
-                    if(atom.type == 2):
+                while(atom.type != Type.NIL):
+                    if(atom.type == Type.PAIR):
                         retStr+=' . '
                         retStr+=str(atom.car())
                         atom = atom.cdr()
@@ -34,22 +36,21 @@ class Data:
                         break
                 retStr+=')'
                 return str(retStr)
-
         else:
             return str(self.value)
         #return "babo~ya"
 
 def cons(d1, d2):
-    return Data(2, (d1, d2))
+    return Data(Type.PAIR, (d1, d2))
 
 def mkint(n):
-    return Data(1, n)
+    return Data(Type.INT, n)
 
 def mksym(s):
-    return Data(4, s)
+    return Data(Type.SYMBOL, s)
 
-def nil():
-    return Data(3, None)
+def nilp():
+    return Data(Type.NIL)
 
 if __name__ == "__main__":
     print("========YR TEST========")
@@ -58,9 +59,9 @@ if __name__ == "__main__":
     print(cons(1, 5).cdr())
     print(mkint(3))
     print(mksym('ABC'))
-    print(nil())
+    print(nilp())
     print("========TESTING========")
     print(mkint(42))
     print(mksym("FOO"))
     print(cons(mksym("X"), mksym("Y")))
-    print(cons(mkint(1), cons(mkint(2), cons(mkint(3), nil()))))
+    print(cons(mkint(1), cons(mkint(2), cons(mkint(3), nilp()))))
